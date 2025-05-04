fc7fdd9ad5b8c885f1365b4fffafaeab9ef0d8ba
rendercold007 <flameboy568@gmail.com>
2025-05-04 15:50:02 +0530

    second commit

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-// script.js
+script.js
 
-// const drumSounds = {
-//     A: "sounds/clap.wav",
-//     S: "sounds/snare.wav",
-//     D: "sounds/tom.wav",
-//     F: "sounds/hihat.wav",
-//     G: "sounds/ride.wav",
-//     H: "sounds/openhat.wav",
-//     J: "sounds/tink.wav",
-//     K: "sounds/boom.wav",
-//     L: "sounds/kick.wav"
-//   };
+const drumSounds = {
+    A: "sounds/clap.wav",
+    S: "sounds/snare.wav",
+    D: "sounds/tom.wav",
+    F: "sounds/hihat.wav",
+    G: "sounds/ride.wav",
+    H: "sounds/openhat.wav",
+    J: "sounds/tink.wav",
+    K: "sounds/boom.wav",
+    L: "sounds/kick.wav"
+  };
   
-//   function playSound(key) {
-//     const sound = drumSounds[key.toUpperCase()]; 
-//     if (sound) {
-//       const audio = new Audio(sound);
-//       audio.play();
+  function playSound(key) {
+    const sound = drumSounds[key.toUpperCase()]; 
+    if (sound) {
+      const audio = new Audio(sound);
+      audio.play();
   
-//       const button = document.querySelector(`button[data-key="${key.toUpperCase()}"]`);
-//       if (button) {
-//         button.classList.add("playing");
-//         setTimeout(() => button.classList.remove("playing"), 150);
-//       }
-//     }
-//   }
+      const button = document.querySelector(`button[data-key="${key.toUpperCase()}"]`);
+      if (button) {
+        button.classList.add("playing");
+        setTimeout(() => button.classList.remove("playing"), 150);
+      }
+    }
+  }
   
-//   document.addEventListener("keydown", (e) => {
-//     playSound(e.key);
-//   });
+  document.addEventListener("keydown", (e) => {
+    playSound(e.key);
+  });
   
-//   document.querySelectorAll(".drum").forEach((button) => {
-//     button.addEventListener("click", () => {
-//       playSound(button.dataset.key);
-//     });
-//   });
+  document.querySelectorAll(".drum").forEach((button) => {
+    button.addEventListener("click", () => {
+      playSound(button.dataset.key);
+    });
+  });
+  //vhvhv
   
